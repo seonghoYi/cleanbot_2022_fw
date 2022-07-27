506d06c61a0a04590d0c40a5e8b74d615620a5f3
seonghoYi <sh.Lee4781@gmail.com>
2022-07-27 20:40:37 +0900

    usb cdc 추가

---
+++ cleanbot_2022_fw/src/common/hw/include/usb.h
@@ -0,0 +1,17 @@
+
+#ifndef SRC_COMMON_HW_INCLUDE_USB_H_
+#define SRC_COMMON_HW_INCLUDE_USB_H_
+
+
+#include "hw_def.h"
+
+
+#ifdef _USE_HW_USB
+
+
+bool usbInit(void);
+
+
+#endif
+
+#endif /* SRC_COMMON_HW_INCLUDE_USB_H_ */
