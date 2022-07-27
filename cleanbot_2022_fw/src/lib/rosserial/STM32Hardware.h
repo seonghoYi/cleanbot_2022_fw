b7963987dc46410846e24e1104b3c8068848d4a0
seonghoYi <sh.Lee4781@gmail.com>
2022-07-27 20:42:15 +0900

    rosserial 통신 개선

--- cleanbot_2022_fw/src/lib/rosserial/STM32Hardware.h
+++ cleanbot_2022_fw/src/lib/rosserial/STM32Hardware.h
@@ -46,6 +46,7 @@
  * 프로젝트와 하드웨어의 상황에 따라 아래 코드를 적절히 수정하여 사용할 것.
  */
 
+extern UART_HandleTypeDef huart2;
 
 class STM32Hardware {
   protected:
@@ -53,14 +54,17 @@ class STM32Hardware {
     const static uint16_t rbuflen = 1024; //수신 버퍼 최대 크기
     const static uint16_t tbuflen = 1024; //송신 버퍼 최대 크기
 
+    uint8_t tbuf[tbuflen];
+    uint32_t twind, tfind;
+
   public:
     STM32Hardware(): //실제로 작동되는 생성자
-      uart_ch(_DEF_UART1) //기본값. 적절히 수정하여 사용할 것.
+      uart_ch(_DEF_UART1), twind(0), tfind(0) //기본값. 적절히 수정하여 사용할 것.
     {
     }
 
     STM32Hardware(uint8_t uart_ch_):
-      uart_ch(uart_ch_)
+      uart_ch(uart_ch_), twind(0), tfind(0)
     {
     }
   
@@ -88,16 +92,37 @@ class STM32Hardware {
 
     }
 
-    void write(uint8_t* data, int length) //512바이트를 초과하는 경우 결과를 보장할 수 없음
+    void flush(void){
+      static bool mutex = false;
+
+      if(!mutex){
+        mutex = true;
+
+        if(twind != tfind){
+          uint16_t len = tfind < twind ? twind - tfind : tbuflen - tfind;
+          if (uartWrite(_DEF_UART1, &(tbuf[tfind]), len) > 0)
+          {
+          	tfind = (tfind + len) & (tbuflen - 1);
+          }
+        }
+        mutex = false;
+      }
+    }
+
+    void write(uint8_t* data, int length) //tbuflen을 초과하는 경우 결과를 보장할 수 없음
     {
-    	//__disable_irq();
-    	int n = length;
-    	n = n <= tbuflen ? n : tbuflen; //입력 최대 크기 체크
+      int n = length;
+      n = n <= tbuflen ? n : tbuflen;
 
+      int n_tail = n <= tbuflen - twind ? n : tbuflen - twind;
+      memcpy(&(tbuf[twind]), data, n_tail);
+      twind = (twind + n) & (tbuflen - 1);
 
-    	uartWrite(_DEF_UART1, data, (uint32_t)n);
-			//uartWrite(_DEF_UART2, data, (uint32_t)n);
-    	//__enable_irq();
+      if(n != n_tail){
+        memcpy(tbuf, &(data[n_tail]), n - n_tail);
+      }
+
+      flush();
     }
 
     unsigned long time()
